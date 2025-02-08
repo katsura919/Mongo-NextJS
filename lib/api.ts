11f907d624b347a16602f52d914b0429cc27875e
Jan Paul <katsuragik919@gmail.com>
2025-02-08 22:28:22 +0800

    added deployed api

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Update with your backend URL
+const LOCAL_BASE_URL = 'http://localhost:5000/api'; 
+const DEPLOYED_BASE_URL = 'https://mongo-backend-4prm.onrender.com/api'; 
 
 export const registerUser = async (email: string, password: string, role: string) => {
-  return axios.post(`${API_BASE_URL}/register`, { email, password, role });
+  return axios.post(`${DEPLOYED_BASE_URL}/register`, { email, password, role });
 };
 
 export const loginUser = async (email: string, password: string) => {
-  return axios.post(`${API_BASE_URL}/login`, { email, password });
+  return axios.post(`${DEPLOYED_BASE_URL}/login`, { email, password });
 };
