4cf9a8d32b2fad07758600531ec6988f6827db08
Jan Paul <katsuragik919@gmail.com>
2025-02-08 22:04:56 +0800

    wew

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
-
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import { Card, CardContent } from "@/components/ui/card";
+import {CardContent } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 
 export default function HomePage() {
